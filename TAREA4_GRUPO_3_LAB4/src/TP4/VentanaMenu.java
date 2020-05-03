3ae665769db0c5e272988e3fed0b945639d8579a
Daniela Rubinstein <nanurubinstein@gmail.com>
2020-05-02 21:00:01 -0300

    Daniela Rubinstein: Ventana menu con su menu

--- TAREA4_GRUPO_3_LAB4/src/TP4/VentanaMenu.java
+++ TAREA4_GRUPO_3_LAB4/src/TP4/VentanaMenu.java
@@ -6,6 +6,9 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
 
 public class VentanaMenu extends JFrame {
 
@@ -20,8 +23,21 @@ public class VentanaMenu extends JFrame {
 	 * Create the frame.
 	 */
 	public VentanaMenu() {
+		setTitle("Programa");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
+		
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		JMenu mnPelicula = new JMenu("Pelicula");
+		menuBar.add(mnPelicula);
+		
+		JMenuItem mniAgregar = new JMenuItem("Agregar");
+		mnPelicula.add(mniAgregar);
+		
+		JMenuItem mniListar = new JMenuItem("Listar");
+		mnPelicula.add(mniListar);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
